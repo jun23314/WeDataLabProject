f4815a87b55d6c6fb416f7dbfd9a96e4af9f1107
jun23314 <dlwnsfml@naver.com>
2024-06-25 12:09:14 +0900

    fix: user entity 수정

--- src/main/java/com/wedatalab/project/domain/User/entity/User.java
+++ src/main/java/com/wedatalab/project/domain/User/entity/User.java
@@ -1,11 +1,15 @@
 package com.wedatalab.project.domain.User.entity;
 
+import com.wedatalab.project.domain.Board.entity.Board;
 import com.wedatalab.project.global.common.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -28,15 +32,28 @@ public class User extends BaseEntity {
     @Comment("유저 나이")
     private Integer age;
 
-    @Comment("유저 id")
+    @Comment("유저 nickname")
     @Column(nullable = false, length = 20)
-    private String userId;
+    private String nickname;
+
+    @Comment("comment relation")
+    @OneToMany(mappedBy = "comment")
+    List<com.wedatalab.project.domain.Comment.entity.Comment> commentList = new ArrayList<>();
+
+
+    @Comment("board relation for 작성자")
+    @OneToMany(mappedBy = "board")
+    private List<Board> boardList = new ArrayList<>();
 
     @Builder
-    public User(Long id, String name, Integer age, String userId) {
+    public User(Long id, String name, Integer age, String nickname,
+        List<com.wedatalab.project.domain.Comment.entity.Comment> commentList,
+        List<Board> boardList) {
         this.id = id;
         this.name = name;
         this.age = age;
-        this.userId = userId;
+        this.nickname = nickname;
+        this.commentList = commentList;
+        this.boardList = boardList;
     }
 }
