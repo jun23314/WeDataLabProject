bb9c542ad6bcf807ab6bd94fe970663f2d4a0a5d
jun23314 <dlwnsfml@naver.com>
2024-06-26 14:24:18 +0900

    add: UserLikesComment entity 생성

---
+++ src/main/java/com/wedatalab/project/domain/User/entity/UserLikesComment.java
@@ -0,0 +1,40 @@
+package com.wedatalab.project.domain.User.entity;
+
+
+import com.wedatalab.project.domain.Comment.entity.Comment;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserLikesComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    public UserLikesComment(Long id, Comment comment, User user) {
+        this.id = id;
+        this.comment = comment;
+        this.user = user;
+    }
+}
