7fbc2e072dd1bc196449a98f0a70978e7695bd57
jun23314 <dlwnsfml@naver.com>
2024-06-26 16:38:02 +0900

    fix: delete method 변경

--- src/main/java/com/wedatalab/project/domain/User/controller/UserRestController.java
+++ src/main/java/com/wedatalab/project/domain/User/controller/UserRestController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,7 +52,7 @@ public class UserRestController {
         return ResponseEntity.ok(userGetResponse);
     }
 
-    @GetMapping("/delete/{user_id}")
+    @DeleteMapping("/delete/{user_id}")
     @Operation(summary = "user 삭제 API", description = "user 정보를 삭제하는 API입니다.")
     public ResponseEntity<String> deleteUser(
         @PathVariable("user_id") Long userId
