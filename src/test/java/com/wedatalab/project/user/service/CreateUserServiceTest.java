d7ede4d9200b02bc3f6437b583894f76ca399cc3
jun23314 <dlwnsfml@naver.com>
2024-06-27 15:23:55 +0900

    remove: 사용하지 않는 import 삭제

--- src/test/java/com/wedatalab/project/user/service/CreateUserServiceTest.java
+++ src/test/java/com/wedatalab/project/user/service/CreateUserServiceTest.java
@@ -7,20 +7,13 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-import com.wedatalab.project.domain.Board.dto.request.BoardUpdateRequest;
-import com.wedatalab.project.domain.Board.entity.Board;
-import com.wedatalab.project.domain.Board.repository.BoardRepository;
-import com.wedatalab.project.domain.Board.service.BoardService;
 import com.wedatalab.project.domain.User.dto.request.UserCreateRequest;
 import com.wedatalab.project.domain.User.entity.User;
 import com.wedatalab.project.domain.User.exception.AlreadyExistUserException;
-import com.wedatalab.project.domain.User.exception.MemberWhoWithdrewException;
-import com.wedatalab.project.domain.User.exception.UserNotFoundException;
 import com.wedatalab.project.domain.User.repository.UserRepository;
 import com.wedatalab.project.domain.User.service.UserService;
 import com.wedatalab.project.global.exception.ErrorCode;
 import java.util.Optional;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -33,6 +26,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @ExtendWith(MockitoExtension.class)
 public class CreateUserServiceTest {
+
     @Mock
     private UserRepository userRepository;
 
@@ -73,7 +67,7 @@ public class CreateUserServiceTest {
 
         @Test
         @DisplayName("이미 가입된 유저는 생성할 수 없다.")
-        void userAlreadyExist_willFail(){
+        void userAlreadyExist_willFail() {
             //given
             Long userId = 1L;
 
